94a328966f148aef2eea9ecb293e6d1110faa326
IgorPolyatskiy <polyatsky.igor@yandex.ru>
2023-01-07 11:28:40 +0700

    Добавил описание, блок-схему и код 3

---
+++ dz_003/Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+string result;
+
+Console.WriteLine("Введите число ");
+a = int.Parse(Console.ReadLine());  
+
+if(a%2==0)
+{
+    result="четное";
+}
+else
+{
+    result="нечетное";
+}
+    
+
+Console.Write("result= "); 
+Console.Write(result); 
