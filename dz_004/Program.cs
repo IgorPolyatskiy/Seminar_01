13bbd26fc00498bc7860024cf69fb7a7b4dcd2a4
IgorPolyatskiy <polyatsky.igor@yandex.ru>
2023-01-07 11:37:46 +0700

    Добавил описание, блок-схему и код 4

---
+++ dz_004/Program.cs
@@ -0,0 +1,13 @@
+﻿int i = 2, a;
+
+Console.WriteLine("Введите целое число: ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Четные числа до {a}");
+Console.WriteLine();
+while (i < a)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    i = i + 2;
+}
